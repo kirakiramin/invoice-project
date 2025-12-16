ca3fd9ccbe299080bb40e65453f4c9aa3501cb0e
MinjungSeo <minjung4149@naver.com>
2025-12-16 15:57:31 +0900

    next.js 업데이트, DB연동

--- src/app/sales-monthly/page.tsx
+++ src/app/sales-monthly/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import React from 'react';
 import HeaderHome from "@/components/header/HeaderHome";
 import ProductAmountSection from "@/components/common/ProductAmountSection";
